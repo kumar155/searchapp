ce0c24b961f75988aa6804fe63244974b3b9153d
sadonthu <sadonthu@gmail.com>
2020-02-20 12:06:06 +0530

    latest

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -3,7 +3,11 @@ import * as actions from "../actions/actions";
 import * as schedules from '../data/schedules.json';
 import * as cities from '../data/cities.json';
 
-
+/**
+ * returns the array of flight details by providing the source & dest
+ * @param {*} state - array of flight details
+ * @param {*} action - unique action
+ */
 export const flightSchedules = (state = [], action) => {
     switch (action.type) {
         case actions.GET_FLIGHT_SCHEDULES:
@@ -13,6 +17,11 @@ export const flightSchedules = (state = [], action) => {
     }
 }
 
+/**
+ * returns the list of cities
+ * @param {*} state - array
+ * @param {*} action - action object
+ */
 export const citites = (state= [], action) => {
     switch (action.type) {
         case actions.GET_CITIES:            
@@ -22,6 +31,11 @@ export const citites = (state= [], action) => {
     }
 }
 
+/**
+ * returns filtered schedules from whole list schedules 
+ * @param {*} schedules - all schedules
+ * @param {*} payload - source & dest object
+ */
 const filterSchedules = (schedules, payload) => {
     return schedules.filter(i =>
         (i.Src === payload.source && i.Dest === payload.destination));
